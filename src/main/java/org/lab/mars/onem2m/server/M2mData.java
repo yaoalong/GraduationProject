690c16c1b13b4f59a47176f85769fc25854211d3
yaoalong <sishimei@163.com>
2016-01-09 14:05:24 +0800

    将序列化的数据发送到数据库中

--- src/main/java/org/lab/mars/onem2m/server/M2mData.java
+++ src/main/java/org/lab/mars/onem2m/server/M2mData.java
@@ -11,62 +11,63 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * M2m内存数据 
+ * M2m内存数据
+ * 
  * @author Administrator
  *
  */
-public class M2mData implements M2mRecord{
-	 /**
+public class M2mData implements M2mRecord {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5084501890442461767L;
 	private static final Logger LOG = LoggerFactory.getLogger(M2mData.class);
-	 private static final ConcurrentHashMap<Integer, M2mDataNode> nodes =
-		        new ConcurrentHashMap<Integer, M2mDataNode>();
-	 public boolean initialized = false;//数据是否初始化
-	 public M2mData(){
-		 M2mDataNode m2mDataNode=new M2mDataNode();
-		 m2mDataNode.setId(1);
-		 m2mDataNode.setLabel(4);
-		 m2mDataNode.setZxid(3);
-		 m2mDataNode.setData(4);
-		 nodes.put(1, m2mDataNode);
-	 }
-	 public void addM2mDataNode(Integer key,M2mDataNode m2mDataNode){
-		 nodes.put(key, m2mDataNode);
-	 }
-	 public Integer getNodeCount(){
-		 return nodes.size();
-	 }
+	private static final ConcurrentHashMap<Integer, M2mDataNode> nodes = new ConcurrentHashMap<Integer, M2mDataNode>();
+	public boolean initialized = false;// 数据是否初始化
+
+	public M2mData() {
+		M2mDataNode m2mDataNode = new M2mDataNode();
+	}
+
+	public void addM2mDataNode(Integer key, M2mDataNode m2mDataNode) {
+		nodes.put(key, m2mDataNode);
+	}
+
+	public Integer getNodeCount() {
+		return nodes.size();
+	}
+
 	@Override
 	public void serialize(M2mOutputArchive archive, String tag)
 			throws IOException {
 		archive.writeInt(nodes.size(), "count");
-		System.out.println("长度是:"+nodes.size());
-		for(Map.Entry<Integer,M2mDataNode> m2mDataNode:nodes.entrySet()){
-			archive.writeInt(m2mDataNode.getKey(),"key");
+		for (Map.Entry<Integer, M2mDataNode> m2mDataNode : nodes.entrySet()) {
+			archive.writeInt(m2mDataNode.getKey(), "key");
 			archive.writeRecord(m2mDataNode.getValue(), "m2mDataNode");
 		}
-		
+
 	}
+
 	@Override
 	public void deserialize(M2mInputArchive archive, String tag)
 			throws IOException {
-		int count = archive.readInt( "count" );
-		System.out.println("序列化出来的长度是:"+count);
+		int count = archive.readInt("count");
 		while (count > 0) {
-				   M2mDataNode m2mDataNode=new M2mDataNode();
-				   Integer key=archive.readInt("key");
-				   System.out.println("key::"+key);
-		          archive.readRecord(m2mDataNode, "m2mDataNode");
-		          nodes.put(key, m2mDataNode);
-		          count--;
-		          
+			M2mDataNode m2mDataNode = new M2mDataNode();
+			Integer key = archive.readInt("key");
+			archive.readRecord(m2mDataNode, "m2mDataNode");
+			nodes.put(key, m2mDataNode);
+			count--;
+
 		}
-		
+
 	}
+
 	public ConcurrentHashMap<Integer, M2mDataNode> getNodes() {
 		return nodes;
 	}
-  
+
+	public void clear() {
+		nodes.clear();
+	}
 }
