d2745fc892a92dc2989994754d34444d622cf9ca
yaoalong <sishimei@163.com>
2016-01-11 17:02:15 +0800

    删除无用代码

--- src/test/java/org/lab/mars/onem2m/consistent/hash/NetworkPoolTest.java
+++ src/test/java/org/lab/mars/onem2m/consistent/hash/NetworkPoolTest.java
@@ -1,15 +1,24 @@
 package org.lab.mars.onem2m.consistent.hash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class NetworkPoolTest {
 	@Test
 	public void testMd5HashingAlg() {
+		NetworkPool networkPool=new NetworkPool();
+	    List<String> servers=new ArrayList<String>();
+	    for(Integer i=0;i<10;i++){
+	    	servers.add("192.168.10.10"+i);
+	    }
+	    networkPool.setServers(servers.toArray(new String[servers.size()]));
+	    networkPool.initialize();
 		String string = "key";
 		for (int i = 0; i < 10; i++) {
 			string += i;
-			System.out.println(NetworkPool.md5HashingAlg(string));
+			System.out.println(networkPool.getSock(string));
 		}
-
 	}
 }
